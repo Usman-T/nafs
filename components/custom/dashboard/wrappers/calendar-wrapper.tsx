193701055aeffbce333c6e97383585ab81004b98
Usman-T <r.usman.tanveer@gmail.com>
2025-05-18 13:53:29 +0500

    fix: Wrapper made asynchronous

--- components/custom/dashboard/wrappers/calendar-wrapper.tsx
+++ components/custom/dashboard/wrappers/calendar-wrapper.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import DashboardCalendar from '../calendar-dashboard'
+import { fetchDailyTasks } from '@/lib/data';
 
-const CalendarWrapper = () => {
+const CalendarWrapper = async () => {
   const dailyTasks = await fetchDailyTasks();
   return (
       <DashboardCalendar dailyTasks={dailyTasks} />
