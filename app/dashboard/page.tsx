847608216a9b3e50f26d5250da211184024fd383
Usman-T <r.usman.tanveer@gmail.com>
2025-05-18 13:34:29 +0500

    feat: Loading states for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,11 @@ import Tasks from "@/components/custom/dashboard/tasks";
 import Dimensions from "@/components/custom/dashboard/dimensions";
 import DashboardCalendar from "@/components/custom/dashboard/calendar-dashboard";
 import StatsCards from "@/components/custom/dashboard/stats-cards";
-import { fetchCurrentChallenge, fetchDailyTasks, fetchUserDimensions } from "@/lib/data";
+import {
+  fetchCurrentChallenge,
+  fetchDailyTasks,
+  fetchUserDimensions,
+} from "@/lib/data";
 import { redirect } from "next/navigation";
 import SpiritualPath from "@/components/custom/dashboard/path-dashboard";
 
@@ -18,9 +22,11 @@ const DashboardPage = async () => {
   return (
     <div className="space-y-8 p-8">
       <SpiritualPath currentLevel={1} currentStreak={3} />
-      <Tasks challenge={currentChallenge} dailyTasks={dailyTasks} />
-      <Dimensions dimensions={dimensionValues}/>
-      <DashboardCalendar />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <Tasks challenge={currentChallenge} dailyTasks={dailyTasks} />
+        <Dimensions dimensions={dimensionValues} />
+      </div>
+      <DashboardCalendar dailyTasks={dailyTasks} />
       <StatsCards />
     </div>
   );
