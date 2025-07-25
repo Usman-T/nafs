19e9ba20e74d244cdf9ae70e7d8216ac38dd033e
Usman-T <r.usman.tanveer@gmail.com>
2025-07-25 15:37:34 +0500

    feat: mobile onboarding

--- components/custom/onboarding/mobile-onboarding.tsx
+++ components/custom/onboarding/mobile-onboarding.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { ChevronRight, ChevronLeft, Sparkles } from "lucide-react";
+import {  Sparkles } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils/utils";
 import Logo from "../logo";
